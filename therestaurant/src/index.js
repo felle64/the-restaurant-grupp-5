6351123f8ebf0e834be9433b2c43d58c6da859b1
felle64 <felle641@gmail.com>
2023-04-18 12:30:59 +0200

    type error

--- therestaurant/src/index.js
+++ therestaurant/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Router.jsx";
+import { router } from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
