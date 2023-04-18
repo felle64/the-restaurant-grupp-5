38d53210dc79cbd6d6b485a6371c6b76dcec9aff
Edots112 <erik.strahle94@gmail.com>
2023-04-18 14:04:17 +0200

    React-router
    added React-router
    
    Co-Authored-By: Ippe <110387491+felipestiernhoff@users.noreply.github.com>
    Co-Authored-By: Felix <86241068+felle64@users.noreply.github.com>

---
+++ therestaurant/src/Router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Menu } from "./components/Menu/Menu";
+import { Layout } from "./components/Layout/Layout";
+
+export const router = createBrowserRouter([
+	{
+		path: "/", // http://localhost:3000/
+		element: <Layout />,
+		children: [
+			{
+				path: "/", // http://localhost:3000/
+				element: <App />,
+				index: true,
+			},
+			{
+				path: "/about", // http://localhost:3000/about
+				element: <About />,
+			},
+			{
+				path: "/contact", // http://localhost:3000/contact
+				element: <Contact />,
+			},
+			{
+				path: "/Menu", // http://localhost:3000/menu
+				element: <Menu />,
+			},
+		],
+	},
+]);
---
+++ therestaurant/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+export const Navigation = () => {
+	return (
+		<nav className='navbar'>
+			<div className="navbar-brand">THI RESTUARNT</div>
+			<ul className='navbar-links'>
+				<li className='navbar-link'>
+					<Link to={"/"}>Hem</Link>
+				</li>
+				<li className='navbar-link'>
+					<Link to={"/about"}>Om oss</Link>
+				</li>
+				<li className='navbar-link'>
+					<Link to={"/menu"}>Meny</Link>
+				</li>
+				<li className='navbar-link'>
+					<Link to={"/contact"}>Kontakt</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
