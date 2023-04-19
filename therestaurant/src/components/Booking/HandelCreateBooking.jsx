49b9faeaf01a6aa66412edf983aaf6165777692a
felle64 <felle641@gmail.com>
2023-04-19 14:01:07 +0200

    its working!!

---
+++ therestaurant/src/components/Booking/HandelCreateBooking.jsx
@@ -0,0 +1,35 @@
+import Web3 from "web3"
+import { CONTRACT_ADDRESS, ABI_ADDRESS } from "../../config";
+import { Booking } from "../../modules/Booking";
+import { useState } from "react";
+
+
+export async function HandleCreateBooking(evt) {
+    console.log(evt);
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    const contractInstance = new web3.eth.Contract(ABI_ADDRESS, CONTRACT_ADDRESS);
+    const account = accounts[0];
+    const contract = contractInstance;
+    const booking = evt;
+    console.log(booking);
+    console.log(contract);
+    console.log(account);
+
+    await contract.methods
+    .createBooking(
+        booking.numberOfGuests,
+        booking.name,
+        booking.date,
+        booking.time,
+        booking.restaurantId
+    )
+    .send({ from: account })
+    .once("receipt", async (receipt) => {
+        console.log(receipt);
+    }
+    );
+
+    
+    
+}
