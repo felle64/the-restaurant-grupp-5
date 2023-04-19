1473f18c38f892962b554a040c150023e31028a8
Edots112 <erik.strahle94@gmail.com>
2023-04-19 18:14:17 +0200

    ContactPage
    Simple ContactPage

---
+++ therestaurant/src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import './Contact.css';
+
+export const Contact = () => {
+
+
+
+  return (
+    <div className='contact-container'>
+      <h1 className='contact-heading'>Kontakta oss</h1>
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">Namn:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        required
+      />
+
+      <label htmlFor="email">E-post:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        required
+      />
+
+      <label htmlFor="message">Meddelande:</label>
+      <textarea
+        id="message"
+        name="message"
+        required
+      />
+
+      <button type="submit">Skicka</button>
+    </form>
+    </div>
+  );
+};
