e5df3de543f024addef315a16495bf26db7958fe
felipestiernhoff <felipehipsman@hotmail.com>
2023-04-27 11:20:54 +0200

    Last design update, values work.

--- therestaurant/src/components/Booking/CreateBookingStep2.jsx
+++ therestaurant/src/components/Booking/CreateBookingStep2.jsx
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import "./CreateBooking.css";
 
 export const CreateBookingStep2 = ({ booking, handleOnChange, handleNextStep, handlePreviousStep, lastName }) => {
 
@@ -13,6 +14,7 @@ export const CreateBookingStep2 = ({ booking, handleOnChange, handleNextStep, ha
             type="text"
             name="firstName"
             id="firstName"
+            className="firstNameClass"
             value={booking.firstName}
             onChange={handleOnChange}
             required
@@ -22,13 +24,15 @@ export const CreateBookingStep2 = ({ booking, handleOnChange, handleNextStep, ha
             type="text"
             name="lastName"
             id="lastName"
+            className="lastNameClass"
             value={lastName}
             onChange={handleOnChange}
             required
           />
-          <button onClick={handlePreviousStep}>Previous</button>
-          <button onClick={handleNextStep}>Next</button>
-
+          <div className="slideButtons">
+          <button id="prevButton" className="prevButtonClass" onClick={handlePreviousStep}>Previous</button>
+          <button id="nextButton" className="nextButtonClass" onClick={handleNextStep}>Next</button>
+          </div>
           </form>
           </>
 
